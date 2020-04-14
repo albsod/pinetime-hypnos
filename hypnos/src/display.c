79777c98d48eb9897037dc0f253cf28c6a9db2a7
Lukas Berglund <lukas.berglund@endian.se>
2020-04-14 11:17:16 +0200

    Added logging for better debugging. Use this instead of printk.

--- hypnos/src/display.c
+++ hypnos/src/display.c
@@ -5,11 +5,11 @@
  */
 
 #include <zephyr.h>
-#include <sys/printk.h>
 #include <drivers/gpio.h>
 #include <drivers/display.h>
 #include <display.h>
 #include "display.h"
+#include "log.h"
 
 /* ********** ********** VARIABLES AND STRUCTS ********** ********** */
 static struct device *display_dev;
@@ -19,7 +19,7 @@ static struct device *display_dev;
 void display_init(void)
 {
 	display_dev = device_get_binding(CONFIG_LVGL_DISPLAY_DEV_NAME);
-	printk("Display initialized.\n");
+	LOG_DBG("Display init: Done");
 }
 
 void display_disable_blanking(void)
