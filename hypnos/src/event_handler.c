7e73e70249d43bd50377d14e89eedad4522ec983
Albin Söderqvist <albin.soderqvist@endian.se>
2020-07-04 14:47:38 +0200

    Add BT toggle lock with timeout

--- hypnos/src/event_handler.c
+++ hypnos/src/event_handler.c
@@ -26,11 +26,12 @@
 #define PULL_UP DT_GPIO_FLAGS(DT_ALIAS(sw0), gpios)
 #define TOUCH_PORT CONFIG_CST816S_NAME
 #define DISPLAY_TIMEOUT K_SECONDS(5)
-#define BT_TIMEOUT K_SECONDS(20)
+#define BT_TOGGLE_LOCK_TIMEOUT K_SECONDS(3)
 /* ********** ******* ********** */
 
 /* ********** variables ********** */
 static struct k_timer display_off_timer;
+static struct k_timer bt_toggle_timer;
 static struct device *charging_dev;
 static struct gpio_callback charging_cb;
 static struct device *button_dev;
@@ -67,6 +68,7 @@ void event_handler_init()
 
 	/* Initialize timers */
 	k_timer_init(&display_off_timer, display_off_isr, NULL);
+	k_timer_init(&bt_toggle_timer, bt_toggle_unlock_isr, NULL);
 
 	/* Start timers */
 	k_timer_start(&display_off_timer, DISPLAY_TIMEOUT, K_NO_WAIT);
@@ -92,6 +94,11 @@ void display_off_isr(struct k_timer *light_off)
 	display_sleep();
 }
 
+void bt_toggle_unlock_isr(struct k_timer *bt_toggle)
+{
+	bt_toggle_unlock();
+}
+
 void battery_charging_isr(struct device *gpiobat, struct gpio_callback *cb, u32_t pins)
 {
 	u32_t res = gpio_pin_get(charging_dev, BAT_CHA);
@@ -105,6 +112,11 @@ void button_pressed_isr(struct device *gpiobtn, struct gpio_callback *cb, u32_t 
 	display_wake_up();
 	clock_show_time();
 	battery_show_status();
+	if (bt_toggle_is_locked()) {
+		return;
+	}
+	k_timer_start(&bt_toggle_timer, BT_TOGGLE_LOCK_TIMEOUT, K_NO_WAIT);
+
 	if (bt_mode()) {
 		gfx_bt_set_label(0);
 		gfx_update();
