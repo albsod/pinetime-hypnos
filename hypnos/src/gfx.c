d555826b796a5900ec2c2e2efacb9d8dc1fb96e9
Albin Söderqvist <albin.soderqvist@endian.se>
2020-07-16 10:47:09 +0200

    gfx: Remove background image and use larger fonts for time and date
    The background image may have been pretty, but with this layout it's
    much easier to read the current time.

--- hypnos/src/gfx.c
+++ hypnos/src/gfx.c
@@ -9,8 +9,9 @@
 #include "log.h"
 
 /* ********** Macros and constants ********** */
-#define LV_IMG_DECLARE(var_name) extern const lv_img_dsc_t var_name;
-LV_IMG_DECLARE(evelyn);
+LV_FONT_DECLARE(rubik_regular_68);
+LV_FONT_DECLARE(rubik_regular_34);
+
 #define BAT_LABEL_MARGIN 3
 /* ********** ******* ********** */
 
@@ -23,36 +24,54 @@ static lv_obj_t *date_label;
 /* ********** Functions ********** */
 void gfx_init(void)
 {
-	/* Text color */
-	lv_style_plain.text.color = LV_COLOR_WHITE;
+	/* Create styles for time, date and the rest */
+	static lv_style_t style;
+	static lv_style_t style_time;
+	static lv_style_t style_date;
+	lv_style_copy(&style, &lv_style_plain);
+	lv_style_copy(&style_time, &lv_style_plain);
+	lv_style_copy(&style_date, &lv_style_plain);
 
-	/* Background image Night and Sleep (public domain)
-	 * See https://en.wikipedia.org/wiki/Night_and_Sleep
-	 */
-	lv_obj_t *img_bin = lv_img_create(lv_scr_act(), NULL);
-	lv_img_set_src(img_bin, &evelyn);
+	/* Default style */
+	style.body.main_color = LV_COLOR_BLACK;
+	style.body.grad_color = LV_COLOR_BLACK;
+	style.text.color = LV_COLOR_WHITE;
+	style.text.font = &lv_font_roboto_22;
+	lv_obj_set_style(lv_scr_act(), &style);
 
 	/* Battery label */
 	battery_label = lv_label_create(lv_scr_act(), NULL);
-	lv_label_set_style(battery_label, LV_LABEL_STYLE_MAIN, &lv_style_plain);
+	lv_label_set_style(battery_label, LV_LABEL_STYLE_MAIN, &style);
 	lv_label_set_text(battery_label, "");
 
 	/* Bluetooth label */
 	bt_label = lv_label_create(lv_scr_act(), NULL);
-	lv_obj_align(bt_label, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+	lv_obj_align(bt_label, NULL, LV_ALIGN_IN_TOP_LEFT, 6, 4);
+	lv_label_set_style(bt_label, LV_LABEL_STYLE_MAIN, &style);
 	lv_label_set_text(bt_label, "");
 
-	/* Time label */
+	/* Time label and style */
+	style_time.body.main_color = LV_COLOR_BLACK;
+	style_time.body.grad_color = LV_COLOR_BLACK;
+	style_time.text.font = &rubik_regular_68;
+	style_time.text.color = LV_COLOR_WHITE;
+	style_time.text.color = LV_COLOR_WHITE;
+	lv_obj_set_style(lv_scr_act(), &style_time);
 	time_label = lv_label_create(lv_scr_act(), NULL);
-	lv_label_set_style(time_label, LV_LABEL_STYLE_MAIN, &lv_style_plain);
+	lv_label_set_style(time_label, LV_LABEL_STYLE_MAIN, &style_time);
 	lv_label_set_text(time_label, "00:00");
-	lv_obj_align(time_label, NULL, LV_ALIGN_CENTER, 0, -10);
+	lv_obj_align(time_label, NULL, LV_ALIGN_CENTER, 0, -25);
 
-	/* Date label */
+	/* Date label and style */
+	style_date.body.main_color = LV_COLOR_BLACK;
+	style_date.body.grad_color = LV_COLOR_BLACK;
+	style_date.text.font = &rubik_regular_34;
+	style_date.text.color = LV_COLOR_YELLOW;
+	lv_obj_set_style(lv_scr_act(), &style_date);
 	date_label = lv_label_create(lv_scr_act(), NULL);
-	lv_label_set_style(date_label, LV_LABEL_STYLE_MAIN, &lv_style_plain);
+	lv_label_set_style(date_label, LV_LABEL_STYLE_MAIN, &style_date);
 	lv_label_set_text(date_label, "Mon 10 Jan");
-	lv_obj_align(date_label, time_label, LV_ALIGN_CENTER, 0, 26);
+	lv_obj_align(date_label, NULL, LV_ALIGN_CENTER, 0, 30);
 	LOG_DBG("Graphics init: Done");
 }
 
@@ -65,11 +84,13 @@ void gfx_update(void)
 void gfx_time_set_label(char *str)
 {
 	lv_label_set_text(time_label, str);
+	lv_obj_align(time_label, NULL, LV_ALIGN_CENTER, 0, -25);
 }
 
 void gfx_date_set_label(char *str)
 {
 	lv_label_set_text(date_label, str);
+	lv_obj_align(date_label, NULL, LV_ALIGN_CENTER, 0, 30);
 }
 
 void gfx_bt_set_label(enum bt_symbol s)
